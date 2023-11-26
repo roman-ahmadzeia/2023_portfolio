687ecef1f303b6cc2554d361f2691cf9259d2131
roman-ahmadzeia <romanahmadzeia@ontariotechu.net>
2023-11-26 13:08:42 -0500

    complete

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// window.addEventListener('scroll', function () {
+//     var quicklinks = document.querySelector('.quicklinks');
+//     var quicklinksOffset = quicklinks.offsetTop;
+//     if (window.scrollY > quicklinksOffset) {
+//         quicklinks.classList.remove('hide-quicklinks');
+//     } else {
+//         quicklinks.classList.add('hide-quicklinks');
+//     }
+// });
+
